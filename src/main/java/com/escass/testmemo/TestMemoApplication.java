68dcdc39a264244b7f1e1118faf217c80b43c619
escurse <av1967@naver.com>
2024-11-15 09:10:50 +0900

    test

---
+++ src/main/java/com/escass/testmemo/TestMemoApplication.java
@@ -0,0 +1,13 @@
+package com.escass.testmemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestMemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestMemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/escass/testmemo/TestMemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.escass.testmemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestMemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
